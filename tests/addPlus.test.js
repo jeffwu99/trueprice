aeb7eeb69bc02be3e5f65dcac99851d76545b18a
remy <rzhang240@gmail.com>
2021-07-01 14:48:54 -0700

    test

--- tests/addPlus.test.js
+++ tests/addPlus.test.js
@@ -1,5 +1,5 @@
 const addPlus = require('../utils').addPlus
-
+//test that I can change things. - Remy
 test('replaces " " with "+" in a string', () =>{
   expect(addPlus("3058 Spuraway Avenue")).toBe("3058+Spuraway+Avenue");
 });
